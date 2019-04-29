c80eff9f32bc53e3a483e9abf1e647f8be340043
jacob broms <jacob.broms@sanocore.se>
2019-04-29 19:09:21 +0200

    second

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-window.onload = function(){}
+window.onload = function(){
+
+	var player = new Tone.Player({
+	  "url" : "seinfeld.mp3",
+	  "autostart" : true,
+	  "loop" : true,
+	  "reverse": false,
+	  "playbackRate": 1
+	}).toMaster();
+
+	player.volume.value = -16;
+
+	var laughter = new Tone.Player("./laugh2.mp3", function(){
+		console.log('done!');
+		Tone.Transport.start();
+	}).toMaster();
+
+	laughter.volume.value = -3;
+
+	Tone.Transport.scheduleRepeat(function(time){
+		console.log('tick');
+		laughter.start();
+	}, 120);
+}
